7e2a663ef0e72dc329ac6e3f05d22e4a279a17e7
Cody House <codyahouse@gmail.com>
2015-04-18 16:49:03 -0400

    added the ability to upload images, tested out

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Rails.application.configure do
 
   #devise, switch out local host for domain in prod
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
